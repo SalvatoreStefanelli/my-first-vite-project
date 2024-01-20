5e4aaa261d7c84e0d4a469967218738e9301eec1
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2024-01-20 12:56:07 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: "Salvatore",
+      background: "purple",
+      movie: "",
+    };
+  },
+  methods: {
+    greetings() {
+      console.log("ciao", this.message);
+    },
+  },
+};
+</script>
+
+<template>
+  <h1 :class="background">{{ message }}</h1>
+  <button v-on:click="greetings">Click me</button>
+  <label for="movie">Movie</label>
+  <input id="movie" type="text" v-model="movie">
+  <p>{{ movie }}</p>
+</template>
+
+<style></style>
